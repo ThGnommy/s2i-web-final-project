f914bfd0a60cee78fa01ad4645c1331e551ecfbf
ThGnommy <th.brandoli@gmail.com>
2020-10-19 18:35:25 +0200

    add api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { MainPage } from "./Pages/MainPage";
+import { FavoritePage } from "./Pages/FavoritePage";
 
 const App = () => {
-  return <div className='App'></div>;
+  return (
+    <>
+      <Router>
+        <Switch>
+          <Route path='/favorites' component={FavoritePage} />
+          <Route path='/' component={MainPage} />
+        </Switch>
+      </Router>
+    </>
+  );
 };
 
 export default App;
--- src/Pages/MainPage/index.js
+++ src/Pages/MainPage/index.js
@@ -1,5 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 export const MainPage = () => {
-  return <>MAIN PAGE</>;
+  const [data, setData] = useState([]);
+  const [query, setQuery] = useState("");
+
+  const myKey = process.env.REACT_APP_PEXELS_KEY;
+
+  const fetchYoutube = async (e) => {
+    e.preventDefault();
+    await axios
+      .get(`https://api.pexels.com/v1/search`, {
+        params: {
+          query: query,
+          per_page: 80,
+        },
+        headers: {
+          Authorization: myKey,
+        },
+      })
+      .then((response) => {
+        console.log(response.data.photos);
+        setData(response.data.photos);
+      });
+  };
+
+  const handleQuery = (e) => {
+    setQuery(e.target.value);
+  };
+
+  return (
+    <>
+      <form onSubmit={fetchYoutube}>
+        <input onChange={handleQuery} type='text' />
+        <button>FETCH</button>
+      </form>
+      {data.map((image) => (
+        <>
+          <img key={image.id} alt={image.id} src={image.src.medium} />
+          <span key={image.photographer_id}>{image.photographer}</span>
+        </>
+      ))}
+    </>
+  );
 };
